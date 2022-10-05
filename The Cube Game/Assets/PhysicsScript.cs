4399b89a0c59fdedc0bc529f033b4ad91a56ad5e
STUDENT John J. O Sullivan <John.J.OSullivan@students.ittralee.ie>
2022-10-05 12:44:34 +0100

    Added Plane

---
+++ The Cube Game/Assets/PhysicsScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsScript : MonoBehaviour
+{
+    Rigidbody ourRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+             ourRigidBody.AddExplosionForce(1000,transform.position+Vector3.down,2);
+         }
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.position += Vector3.down;
+    }
+}
